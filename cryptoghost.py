9b0e837a8d7f3408f0d82afd20266bd98a3bd430
Abdelkarim Mouchquelita <53612008+mchklt@users.noreply.github.com>
2021-06-25 18:20:11 +0100

    Update cryptoghost.py

--- cryptoghost.py
+++ cryptoghost.py
@@ -1,24 +1,26 @@
 import hashlib
+import os
 import requests
 import base64
 import platform 
 import sys
 from bs4 import BeautifulSoup
 def script():
+    if platform.system().startswith("Linux") :
+        red , green , yellow , blue , endc = '\033[91m' , '\033[92m' ,'\033[93m' , '\033[94m' , '\033[0m'
+    else :
+        red = green = yellow = blue = endc = ""
+    global Banner
+    Banner = """\n
+    #                       _              _               _   
+    #  ___ _ __ _   _ _ __ | |_ ___   __ _| |__   ___  ___| |_ 
+    # / __| '__| | | | '_ \| __/ _ \ / _` | '_ \ / _ \/ __| __|
+    #| (__| |  | |_| | |_) | || (_) | (_| | | | | (_) \__ \ |_ 
+    # \___|_|   \__, | .__/ \__\___/ \__, |_| |_|\___/|___/\__|
+    #           |___/|_|             |___/                     
+    #
+    #                By:\033[91mAbdelkarim Mouchquelita \n\n"""
     try:
-        if platform.system().startswith("Linux") :
-            red , green , yellow , blue , endc = '\033[91m' , '\033[92m' ,'\033[93m' , '\033[94m' , '\033[0m'
-        else :
-            red = green = yellow = blue = endc = ""
-        Banner = """\n
-        #                       _              _               _   
-        #  ___ _ __ _   _ _ __ | |_ ___   __ _| |__   ___  ___| |_ 
-        # / __| '__| | | | '_ \| __/ _ \ / _` | '_ \ / _ \/ __| __|
-        #| (__| |  | |_| | |_) | || (_) | (_| | | | | (_) \__ \ |_ 
-        # \___|_|   \__, | .__/ \__\___/ \__, |_| |_|\___/|___/\__|
-        #           |___/|_|             |___/                     
-        #
-        #                By:\033[91mAbdelkarim Mouchquelita \n\n"""
         text = sys.argv[2].encode("utf-8")
         if sys.argv[1] == "-e":
             print(Banner)
@@ -61,30 +63,34 @@ def script():
             print(Banner)
             hashtxt = sys.argv[2]
             if len(hashtxt) == int(32):
-                url = 'https://gist.githubusercontent.com/mchklt/891807e44bb0f97fb5dcf9131bad0a36/raw/2cffc0ceaeaa2ce1a9334b7931d2b3ef9e450728/mchklt_Words.txt'
-                def hash(txt):
-                    return hashlib.md5(txt.encode("utf-8")).hexdigest()
-                lis_t = requests.get(url).text.split("\n")
-                for text in lis_t:
-                    if hash(text) == hashtxt :
-                        print(green + "[+] Hash Found: " + red + text + "\n\n")
-                        with open("decrypted.txt", 'a') as f:
-                            f.write(f"{hashtxt}:{text}    ----- MD5 -----\n")
-                        print(green + "The result saved in decrypted.txt , enjoy<3\n@MCHKLT")
-                        break
-                    elif hash(text) != hashtxt:
-                        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-                        url = 'https://hashtoolkit.com/decrypt-md5-hash/'
-                        es = requests.get(url + hashtxt, headers = headers).text
-                        soup = BeautifulSoup(es, 'html.parser')
-                        s = soup.find_all('code')
-                        print(green + "[+] Hash Found: " + red + s[1].text + "\n\n")
-                        with open("decrypted.txt", 'a') as f:
-                            f.write(f"{hashtxt}:{s[1].text}    ----- MD5 -----\n")
-                        print(green + "The result saved in decrypted.txt , enjoy<3\n@MCHKLT")
-                        break 
-                    else:
-                        print("Hash Not Found !")
+                try:
+                    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+                    url = 'https://hashtoolkit.com/decrypt-md5-hash/'
+                    es = requests.get(url + hashtxt, headers = headers).text
+                    soup = BeautifulSoup(es, 'html.parser')
+                    s = soup.find_all('code')
+                    print(green + "[+] Hash Found: " + red + s[1].text + "\n\n")
+                    with open("decrypted.txt", 'a') as f:
+                        f.write(f"{hashtxt}:{s[1].text}    ----- MD5 -----\n")
+                    print(green + "The result saved in decrypted.txt , enjoy<3\n@MCHKLT")
+                    exit()
+                except:
+                    pass
+                try:
+                    check = lambda: True if sys.argv[3] == "-u" else False
+                    if check():
+                        lis_t = requests.get(sys.argv[4]).text.split("\n")
+                        def hash(txt):
+                            return hashlib.md5(txt.encode("utf-8")).hexdigest()
+                        for text in lis_t:
+                            if hash(text) == hashtxt :
+                                print(green + "[+] Hash Found: " + red + text + "\n\n")
+                                with open("decrypted.txt", 'a') as f:
+                                    f.write(f"{hashtxt}:{text}    ----- MD5 -----\n")
+                                print(green + "The result saved in decrypted.txt , enjoy<3\n@MCHKLT")
+                                break
+                except:
+                    pass
             elif len(hashtxt) == int(40):
                 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
                 url = 'https://hashtoolkit.com/decrypt-hash/?hash='
@@ -136,6 +142,6 @@ def script():
                     with open("decrypted.txt", 'a') as f:
                         f.write(f"{hashtxt}:{base64.b32decode(hashtxt).decode('ascii')}   ----- BASE64 -----\n")
                     print(green + "The result saved in decrypted.txt , enjoy<3\n@MCHKLT")
-    except IndexError:
-        print(Banner + "\n python3 cryptoghost.py -e [text]\n python3 cryptoghost.py -d [hash]\n\n")
+    except:
+        print(endc+Banner + "\n python3 cryptoghost.py -e [TEXT]\n python3 cryptoghost.py -d [HASH]\n python3 cryptoghost.py -d [HASH] -u [WORDLIST_URL]\n" + blue + "example:\n" + green + " python3 cryptoghost.py -d ddcdfbdd755fe5fec76466e4f881d0b6 -u https://pastebin.com/wordlist.txt\n")
 script()
